0738ad8401d150917825b798d51506334afe4d41
Matthew N <mn.gf@outlook.com>
2025-05-17 18:00:35 +0100

    add animation to star ratings

--- src/components/Steps/Rating.tsx
+++ src/components/Steps/Rating.tsx
@@ -1,4 +1,4 @@
-import { div } from "motion/react-client";
+import { motion } from "motion/react";
 import Star from "../Icons/Star";
 
 interface RatingProps {
@@ -10,7 +10,11 @@ interface RatingProps {
 
 export default function Rating(props: RatingProps) {
   return (
-    <div
+    <motion.div
+    initial={{opacity: 0, y: 100, scale: 0.95}}
+    whileInView={{opacity: 1, y: 0, scale: 1}}
+    viewport={{once: true, amount: 0.9}}
+    transition={{duration: 0.5, ease: "easeInOut"}}
       className={`absolute flex gap-x-2.5 rounded-2xl bg-white/85 py-3.5 pr-5 pl-3 drop-shadow-[0px_4px_30px_rgba(0,0,0,0.12)] backdrop-blur-3xl ${props.className}`}
     >
       <div className="bg-grey-300 flex items-end justify-center overflow-hidden rounded-full">
@@ -30,6 +34,6 @@ export default function Rating(props: RatingProps) {
           </p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
