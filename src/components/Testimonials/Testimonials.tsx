abb45f92816934c3546246a8343f434f402ee607
Matthew N <mn.gf@outlook.com>
2025-05-20 22:27:09 +0100

    fix typo

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -5,7 +5,7 @@ export default function Testimonials() {
   return (
     <section className="px-24 py-36">
         <div className="m-auto max-w-389">
-            <h2 className="mb-22 text-center text-[3.875rem] font-semibold">What Out Vacationers Say</h2>
+            <h2 className="mb-22 text-center text-[3.875rem] font-semibold">What Our Vacationers Say</h2>
             <ul className="flex gap-x-20">
                 {testimonials.map(testimonial =>
                     <Testimonial testimonial={testimonial} key={testimonial.id} />
