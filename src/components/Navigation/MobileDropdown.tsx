528908c966098b0019f5aab61358fc6b5f072d18
Matthew N <mn.gf@outlook.com>
2025-05-23 15:59:54 +0100

    add open and close functionality to mobile menu

--- src/components/Navigation/MobileDropdown.tsx
+++ src/components/Navigation/MobileDropdown.tsx
@@ -11,11 +11,11 @@ export default function MobileDropdown({ links }: MobileDropdownProps) {
       initial={{ opacity: 0, height: 0 }}
       animate={{ opacity: 1, height: "auto" }}
       exit={{ height: 0, opacity: 0 }}
-      transition={{duration: 0.35, ease: "easeInOut"}}
+      transition={{ duration: 0.35, ease: "easeInOut" }}
       className="z-10 flex flex-col gap-y-6 overflow-hidden pl-3"
     >
       {links?.map((link) => (
-        <li className="text-grey-600">
+        <li className="text-grey-600" key={link.id}>
           <a href="#">{link.text}</a>
         </li>
       ))}
